dcd74de0e63428f1276cab90608c6a9bf70e4b9e
Mohamad alavi <Mohamad alavi@DESKTOP-9QCQADM>
2023-07-03 05:13:53 -0700

    Add project files.

---
+++ mazedfs/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mazedfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("radif ra vared konid");
+            int radif = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("soton ra vared konid");
+            int soton = Convert.ToInt32(Console.ReadLine());
+
+            int[,] maze = sakhtmaze(radif, soton);
+
+            
+            
+            rahmaz_dfs test = new rahmaz_dfs(maze);
+
+
+            if (test.hal())
+            {
+                Console.WriteLine("masir zir vojod darad:");
+                test.print();
+            }
+            else
+            {
+                Console.WriteLine("masiri vojod nadarad.");
+            }
+            Console.ReadKey();
+        }
+
+        private static int[,] sakhtmaze(int x, int y)
+        {
+            int[,] maze = new int[x, y];
+            Random divar = new Random();
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    maze[i, j] = divar.Next(2); //ساخت ماز با اعداد رندوم 0 و 1
+                    
+                }
+            }
+
+            return maze;
+        }
+
+
+    }
+}
---
+++ mazedfs/rahmaz_dfs.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mazedfs
+{
+    class rahmaz_dfs
+    {
+
+
+        private int[,] maze;
+        private int radif;
+        private int soton;
+
+        public int s
+        {
+            get
+            {
+                return soton;
+            }
+            set
+            {
+                soton = value;
+            }
+        }
+        public int r
+        {
+            get
+            {
+                return radif;
+            }
+            set
+            {
+                radif = value;
+            }
+        }
+
+        public rahmaz_dfs() { }
+        public rahmaz_dfs(int[,] Maze)
+        {
+            maze = Maze;
+            radif = maze.GetLength(0);
+            soton = maze.GetLength(1);
+        }
+
+        public bool hal()
+        {
+            return masir(0, 0);
+        }
+
+        private bool masir(int x, int y)
+        {
+            if (x < 0 || x >= radif || y < 0 || y >= soton || maze[x, y] == 0)
+            {
+                return false; // مشخص شدن دیوار
+            }
+
+            if (maze[x, y] == 2)
+            {
+                return false; // قبلا چک شده
+            }
+
+            if (x == radif - 1 && y == soton - 1)
+            {
+                maze[x, y] = 2; // پایان
+                return true;
+            }
+
+            maze[x, y] = 2; // بخشی از مسیر
+
+            if (masir(x - 1, y)) // حرکت بالا
+                return true;
+            if (masir(x, y + 1)) // حرکت راست
+                return true;
+            if (masir(x + 1, y)) // حرکت پایین
+                return true;
+            if (masir(x, y - 1)) // حرکت چپ
+                return true;
+
+            maze[x, y] = 1; // عقب نشینی
+            return false;
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < radif; i++)
+            {
+                for (int j = 0; j < soton; j++)
+                {
+                    Console.Write(maze[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+
+}
